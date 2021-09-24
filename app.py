5796b4d812910b256b1bcf25e9f6d497bfb505e5
mikeacosta <mike@acosta.net>
2021-09-24 06:57:01 -0700

    initial commit

---
+++ app.py
@@ -0,0 +1,31 @@
+from flask import Flask, render_template
+import database.db_connector as db
+from flask import Flask, render_template, json # add `json`
+import os
+
+# Configuration
+
+app = Flask(__name__)
+db_connection = db.connect_to_database()
+
+# Routes 
+
+@app.route('/')
+def root():
+    return render_template("main.j2")
+
+@app.route('/bsg-people')
+def bsg_people():
+    query = "SELECT * FROM bsg_people;"
+    cursor = db.execute_query(db_connection=db_connection, query=query)
+    results = cursor.fetchall()
+    return render_template("bsg.j2", bsg_people=results)  
+
+# Listener
+
+if __name__ == "__main__":
+    port = int(os.environ.get('PORT', 1599)) 
+    #                                 ^^^^
+    #              You can replace this number with any valid port
+    
+    app.run(port=port, debug=True) 
